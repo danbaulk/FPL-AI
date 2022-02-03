a8d1ad62812820f8f3617c831c7a773e2b7497c1
DanBaulk <danielbaulk@gmail.com>
2022-02-03 16:36:27 +0000

    split the datasets

--- ann_trainer.py
+++ ann_trainer.py
@@ -1,9 +1,15 @@
 """this module will be used to train the ANN models"""
+import csv
 import ANN
+import numpy as np
 
-myann = ANN.ANN(5)
-myann.printModel()
-myann.saveModel("myfirstann")
+# read data into a list
+with open('Data/GK.csv', newline='') as Data:
+    reader = csv.reader(Data)
+    DataSet = list(reader)
 
-mynewann = ANN.ANN("myfirstann")
-mynewann.printModel()
+# split the data into the different sets based on the ratio
+ratio = [0.6, 0.2, 0.2]
+training_r, validation_r, data_r = ratio
+indicies_for_splitting = [int(len(DataSet) * training_r), int(len(DataSet) * (training_r + validation_r))]
+TrainingData, ValidationData, TestData = np.split(DataSet, indicies_for_splitting)
