d859fd6b0e8085cc5f963c6252cb7ed165cfa34c
DannyBoy173 <danielbaulk@gmail.com>
2022-03-24 12:51:22 +0000

    handle blanks and doubles

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,5 +1,4 @@
 """this module retrieves the data for the upcoming gameweek of all the players"""
-from fileinput import filename
 import Player
 import requests
 import csv
@@ -7,6 +6,9 @@ import json
 from understat import Understat
 import asyncio
 import aiohttp
+
+# get the upcoming gameweek from the user
+upcomingGW = input("Input the gameweek: ")
 
 # list of missing FPL - Understat conversion
 missingIDs = []
@@ -82,79 +84,87 @@ players = response['elements']
 # for each player who is available to play, create a player object and add their details
 for player in players:
     if player['status'] == 'a':
-        ID = 0
-        influence = []
-        creativity = []
-        threat = []
-        ict = []
-        performances = []
-        xG = []
-        xA = []
-        xGC = []
-        team = 0
-        opp = 0
-        home = 0
-
-        playerObj = Player.Player()
-        playerObj.name = player['first_name'] + " " + player['second_name']
-        playerObj.value = player['now_cost']
-        playerObj.pos = pos_dict[player['element_type']]
         ID = player['id']
-            
-        # get their recent FPL performance stats
         url = 'https://fantasy.premierleague.com/api/element-summary/' + str(ID) + '/' 
         response = get(url)
-        history = response['history']
-        recentGames = history[-4:]
+        fixtures = response['fixtures'] # list of all the players next fixtures
+        upcomingFixtures = [] # the upcoming gameweek fixtures for the player
 
-        for game in recentGames:
-            influence.append(float(game['influence']))
-            creativity.append(float(game['creativity']))
-            threat.append(float(game['threat']))
-            ict.append(float(game['ict_index']))
-            performances.append(int(game['total_points']))
+        # get the players upcoming gameweeks fixtures
+        for fixture in fixtures:
+            if fixture['event'] == int(upcomingGW):
+                upcomingFixtures.append(fixture)
         
-        playerObj.avg_I = Player.calcAvg(influence)
-        playerObj.avg_C = Player.calcAvg(creativity)
-        playerObj.avg_T = Player.calcAvg(threat)
-        playerObj.avg_ICT = Player.calcAvg(ict)
-        playerObj.form = Player.calcAvg(performances)
-
-        # get their upcoming fixture details
-        fixture = response['fixtures'][0]
-        if fixture['is_home']:
-            opp = fixture['team_a']
-            team = fixture['team_h']
-            home = 1
-        else:
-            opp = fixture['team_h']
-            team = fixture['team_a']
+        # create a player object for each fixture the player has (usually one, but maybe 2 or none. Rarely 3)
+        for fixture in upcomingFixtures:
+            influence = []
+            creativity = []
+            threat = []
+            ict = []
+            performances = []
+            xG = []
+            xA = []
+            xGC = []
+            team = 0
+            opp = 0
             home = 0
-        team = team_dict[team]
-        playerObj.fixture = rating2021[team_dict[opp]]
-        playerObj.wasHome = home
 
-        # get their recent understat performance stats
-        try:
-            season = 2021
-            understatID = id_dict[str(ID)]
-            loop = asyncio.get_event_loop()
-            retrievedData = loop.run_until_complete(main(season, understatID, team))
+            playerObj = Player.Player()
+            playerObj.name = player['first_name'] + " " + player['second_name']
+            playerObj.value = player['now_cost']
+            playerObj.pos = pos_dict[player['element_type']]
+                
+            # get their recent FPL performance stats
+            url = 'https://fantasy.premierleague.com/api/element-summary/' + str(ID) + '/' 
+            response = get(url)
+            history = response['history']
+            recentGames = history[-4:]
 
-            playerObj.avg_xG = Player.calcAvg(retrievedData[0])
-            playerObj.avg_xA = Player.calcAvg(retrievedData[1])
-            playerObj.avg_xGC = Player.calcAvg(retrievedData[2])
+            for game in recentGames:
+                influence.append(float(game['influence']))
+                creativity.append(float(game['creativity']))
+                threat.append(float(game['threat']))
+                ict.append(float(game['ict_index']))
+                performances.append(int(game['total_points']))
+            
+            playerObj.avg_I = Player.calcAvg(influence)
+            playerObj.avg_C = Player.calcAvg(creativity)
+            playerObj.avg_T = Player.calcAvg(threat)
+            playerObj.avg_ICT = Player.calcAvg(ict)
+            playerObj.form = Player.calcAvg(performances)
 
-            Player.playerDB.append(playerObj) # add the player to the DB
+            # process their upcoming fixture details
+            if fixture['is_home']:
+                opp = fixture['team_a']
+                team = fixture['team_h']
+                home = 1
+            else:
+                opp = fixture['team_h']
+                team = fixture['team_a']
+                home = 0
+            team = team_dict[team]
+            playerObj.fixture = rating2021[team_dict[opp]]
+            playerObj.wasHome = home
 
-        except:
-            # if the players understat data cannot be retrieved (understat ID not in dict) add them to the log
-            missingIDs.append([playerObj.name, ID])
+            # get their recent understat performance stats
+            try:
+                season = 2021
+                understatID = id_dict[str(ID)]
+                loop = asyncio.get_event_loop()
+                retrievedData = loop.run_until_complete(main(season, understatID, team))
+
+                playerObj.avg_xG = Player.calcAvg(retrievedData[0])
+                playerObj.avg_xA = Player.calcAvg(retrievedData[1])
+                playerObj.avg_xGC = Player.calcAvg(retrievedData[2])
+
+                Player.playerDB.append(playerObj) # add the player to the DB
+
+            except:
+                # if the players understat data cannot be retrieved (understat ID not in dict) add them to the log
+                missingIDs.append([playerObj.name, ID])
 
 
 # with all the players data in the playerDB output it to their respective files depending on their positions
-filename = input("Input the gameweek: ")
-
 header = ['name', 'pos', 'avg_xG', 'avg_xA', 'avg_xGC', 'avg_I', 'avg_C', 'avg_T', 'avg_ICT', 'fixture_difficulty', 'is_home', 'form', 'class']
 GK_data = []
 DEF_data = []
@@ -171,25 +181,25 @@ for player in Player.playerDB:
     else:
         FWD_data.append([player.name, player.pos, player.avg_xG, player.avg_xA, player.avg_xGC, player.avg_I, player.avg_C, player.avg_T, player.avg_ICT, player.fixture, player.wasHome, player.form, '?'])
 
-with open('GK_gameweek_' + filename + '.csv', 'w', encoding="utf-8", newline='') as f:
+with open('GK_gameweek_' + upcomingGW + '.csv', 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for row in GK_data:
         writer.writerow(row)
 
-with open('DEF_gameweek_' + filename + '.csv', 'w', encoding="utf-8", newline='') as f:
+with open('DEF_gameweek_' + upcomingGW + '.csv', 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for row in DEF_data:
         writer.writerow(row)
 
-with open('MID_gameweek_' + filename + '.csv', 'w', encoding="utf-8", newline='') as f:
+with open('MID_gameweek_' + upcomingGW + '.csv', 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for row in MID_data:
         writer.writerow(row)
 
-with open('FWD_gameweek_' + filename + '.csv', 'w', encoding="utf-8", newline='') as f:
+with open('FWD_gameweek_' + upcomingGW + '.csv', 'w', encoding="utf-8", newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for row in FWD_data:
@@ -197,7 +207,7 @@ with open('FWD_gameweek_' + filename + '.csv', 'w', encoding="utf-8", newline=''
 
 if len(missingIDs) > 0 :
     # if there are any missing understat IDs in the dictionary then output them to a csv so it can be resolved
-    with open('MissingIDs_' + filename + '.csv', 'w', encoding="utf-8", newline='') as f:
+    with open('MissingIDs_' + upcomingGW + '.csv', 'w', encoding="utf-8", newline='') as f:
         writer = csv.writer(f)
         for row in missingIDs:
             writer.writerow(row)
